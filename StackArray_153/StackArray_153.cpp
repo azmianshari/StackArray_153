86dd41b7127523cea402bd8e8af88e912d0e13d0
azmianshari <azmianshari123@gmail.com>
2023-06-16 14:14:07 +0700

    Membuat fungsi untuk menyimpan data

--- StackArray_153/StackArray_153.cpp
+++ StackArray_153/StackArray_153.cpp
@@ -16,32 +16,4 @@ public:
 		string element;
 		getline(cin, element);
 
-		if (top == 4) { //Step 1
-			cout << "Number of data exceeds the limit." << endl;
-			return;
-		}
-		top++; //Step 2
-		stack_array[top] + element; //Step 3
-		cout << endl;
-		cout << element << "Ditambahkan(Pushed)" << endl;
 		
-	};
-	void pop() {
-		if(empty()) { //Step 1
-			cout << "\nStack is empty. Cannot pop." << endl; //Step 1.a
-			return; //Step 1.b
-		}
-		cout << "\nThe popped element is :" << stack_array[top] << endl; //Step 2
-		top--; //Step 3
-
-	}
-};
-//Method for check if datas is empty
-bool empty() {
-
-}
-
-
-
-	
-
